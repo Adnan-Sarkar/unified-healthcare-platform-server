b7c4b112359ab704a1b8a178d93e5ae6c0ad683d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-09-26 20:52:56 +0600

    Create hospital type and update types index

---
+++ src/app/types/hospital.ts
@@ -0,0 +1,8 @@
+export type THospital = {
+  name: string;
+  area: string;
+  district: string;
+  location: string;
+  website: string;
+  contactNumber: string;
+};
--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -3,3 +3,4 @@ export * from "./jwtPayload";
 export * from "./users";
 export * from "./users";
 export * from "./medicine";
+export * from "./hospital";
