761d0e7e2281d5559aead92d9eebf029fe64940a
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-09-28 11:01:48 +0600

    Update jwtPayload type

--- src/app/types/jwtPayload.ts
+++ src/app/types/jwtPayload.ts
@@ -1,9 +1,8 @@
-import { UserRole } from "./userRole";
 import { TAccountStatus } from "./users";
 
 export type TJWTPayload = {
   id: string;
   accountStatus: TAccountStatus;
   email: string;
-  role: keyof typeof UserRole;
+  roles: string[];
 };
