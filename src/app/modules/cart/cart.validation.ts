fa305c4ef73e9efa78900f5317f978f68935767d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-09-29 23:31:24 +0600

    Create cart validation using zod

---
+++ src/app/modules/cart/cart.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+// add item to cart validation
+const addItemToCartValidationSchema = z.object({
+  medicineId: z.string({
+    required_error: "Medicine ID is required",
+  }),
+  quantity: z
+    .number({
+      required_error: "Quantity is required",
+    })
+    .int()
+    .positive({
+      message: "Quantity must be a positive integer",
+    }),
+});
+
+export const cartValidationSchema = {
+  addItemToCartValidationSchema,
+};
